430223a70b1fabcc65c058f3b6909fd121451ba5
jvieirah <me.contrate.aero@gmail.com>
2023-08-03 21:51:22 -0300

    cc

--- src/main/java/com/trybe/acc/java/OperacoesVeiculos.java
+++ src/main/java/com/trybe/acc/java/OperacoesVeiculos.java
@@ -1,5 +1,25 @@
 package com.trybe.acc.java;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author joao
+ *
+ */
 public class OperacoesVeiculos {
 
+  public <T> int numVeiculosDiferentesVendidos(Stream<T> veiculosVendidos) {
+    long nro = veiculosVendidos.distinct().count();
+    return Math.toIntExact(nro);
+  }
+
+  public <T> Stream<String> obterRepresentacao(Stream<T> veiculos) {
+    return veiculos.map(veiculo -> veiculo.toString());
+  }
+
+  public <T> List<T> obterLista(Stream<T> veiculos) {
+    return veiculos.collect(Collectors.toList());
+  }
 }
